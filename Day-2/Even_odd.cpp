af60a9ee0905d685330c0f4abb02b3fa173c7b63
Neveditha-Govind-Raj <f20251262@hyderabad.bits-pilani.ac.in>
2026-01-12 07:40:41 +0000

    Even_Odd

---
+++ Day-2/Even_odd.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1;
+    
+    cout<<"Enter number:"<<endl;
+    cin>>num1;
+    num1%2==0 ? cout<<"EVEN" : cout<<"ODD";
+    
+return 0;
+
+}
